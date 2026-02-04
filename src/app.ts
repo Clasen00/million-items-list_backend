13cf6e1c3e33d1ff179cf4c9e5aa24936059667b
Eugene Shalaev <anarxict97@mail.ru>
2026-02-04 20:00:31 +1000

    feat: исправлены ошибки cors, обновлен README.md

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
+
+const allowedOrigins = ["https://million-items-list.onrender.com/"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  }),
+);
 
 // Middleware
 app.use(express.json());
